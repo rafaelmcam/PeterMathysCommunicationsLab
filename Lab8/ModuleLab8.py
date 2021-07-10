825abc854a225e328651fce4f56d8b184321b27d
Rafael Campello <rafael.campello@hotmail.com>
2021-07-10 10:07:44 -0300

    Done Lab8: E2A

--- Lab8/ModuleLab8.py
+++ Lab8/ModuleLab8.py
@@ -5,6 +5,84 @@ from Resources import comsig
 from ModuleLab6 import pam12, pamrcvr10
 
 
+def fskxmtr(M,sig_dn,Fs,ptype,pparms,xtype,fcparms):
+    """ M-ary Frequency Shift Keying (FSK) Transmitter for Choherent (’coh’) and Non-coherent (’noncoh’) FSK Signals
+    >>>>> sig_xt = fskxmtr(M,sig_dn,Fs,ptype,pparms,xtype,fcparms) <<<<<
+    where
+    sig_xt: waveform from class sigWave
+    sig_xt.signal(): transmitted FSK signal, sampling rate Fs
+    sig_xt.timeAxis(): time axis for x(t), starts at t=-TB/2
+    M: number of distinct symbol values in d[n]
+    xtype: Transmitter type from set {’coh’,’noncoh’}
+    sig_dn: sequence from class sigSequ sig_dn.signal() = [dn] for [’coh’]
+    sig_dn.signal() = [[dn],[thetacn]] for [’noncoh’]
+    dn: M-ary (0,1,..,M-1) N-symbol DT input sequence d_n
+    thetacn: N-symbol DT sequence theta_c[n] in degrees, used instead of thetac0..thetacM-1 for {’noncoh’} FSK
+    sig_dn.get_FB(): baud rate of d_n (and theta_c[n]), TB=1/FB
+    Fs: sampling rate of x(t)
+    ptype: pulse type from set {’man’,’rcf’,’rect’,’rrcf’,’sinc’,’tri’}
+    pparms = [] for {’man’,’rect’,’tri’}
+    pparms = [k alpha] for {’rcf’,’rrcf’}
+    pparms = [k beta] for {’sinc’}
+    k: "tail" truncation parameter for {’rcf’,’rrcf’,’sinc’}
+    (truncates p(t) to -k*TB <= t < k*TB)
+    alpha: Rolloff parameter for {’rcf’,’rrcf’}, 0<=alpha<=1
+    beta: Kaiser window parameter for {’sinc’}
+    fcparms = [[fc0,fc1,...,fcM-1],[thetac0,thetac1,...,thetacM-1]] for {’coh’}
+    fcparms = [fc0,fc1,...,fcM-1] for {’noncoh’}
+    fc0,fc1,...,fcM-1: FSK (carrier) frequencies for {’coh’,’noncoh’}
+    thetac0,thetac1,...,thetacM-1: FSK (carrier) phases in deg (0: cos, -90: sin) for {'coh'}
+    """
+
+    FB = sig_dn.get_FB()
+    if xtype == 'coh':
+        vec = np.where(sig_dn.signal() == 0, 1, 0)
+        smi = pam12(comsig.sigSequ(vec, FB), Fs, ptype, pparms)
+
+        xf = np.zeros(shape = (len(smi.signal()),))
+        for i in range(M):
+            fci, thetai = fcparms[0][i], fcparms[1][i]
+            vec = np.where(sig_dn.signal() == i, 1, 0)
+            smi = pam12(comsig.sigSequ(vec, FB), Fs, ptype, pparms)
+            xm = smi.signal() * np.cos(2 * np.pi * fci * smi.timeAxis() + thetai * np.pi / 180)
+            xf += xm
+    elif xtype == 'noncoh':
+        vec = np.where(sig_dn.signal() == 0, 1, 0)
+        smi = pam12(comsig.sigSequ(vec, FB), Fs, ptype, pparms)
+
+        L = len(sig_dn.signal())
+        xf = np.zeros(shape = (len(smi.signal()),))
+        theta_vec = 360 * np.random.rand(L)
+        ntheta = np.repeat(theta_vec, int(Fs / FB))
+        for i in range(M):
+            fci = fcparms[i]
+            vec = np.where(sig_dn.signal() == i, 1, 0)
+            smi = pam12(comsig.sigSequ(vec, FB), Fs, ptype, pparms)
+            xm = smi.signal() * np.cos(2 * np.pi * fci * smi.timeAxis() + ntheta * np.pi / 180)
+            xf += xm
+    elif xtype == 'fixed_noncoh':
+        vec = np.where(sig_dn.signal() == 0, 1, 0)
+        smi = pam12(comsig.sigSequ(vec, FB), Fs, ptype, pparms)
+
+        L = len(sig_dn.signal())
+        xf = np.zeros(shape = (len(smi.signal()),))
+        theta_vec = fcparms[1]
+        ntheta = np.repeat(theta_vec, int(Fs / FB))
+        for i in range(M):
+            fci = fcparms[0][i]
+            vec = np.where(sig_dn.signal() == i, 1, 0)
+            smi = pam12(comsig.sigSequ(vec, FB), Fs, ptype, pparms)
+            xm = smi.signal() * np.cos(2 * np.pi * fci * smi.timeAxis() + ntheta * np.pi / 180)
+            xf += xm
+    else:
+        raise NotImplementedError("xtype deve ser 'coh' ou 'noncoh'")
+
+    return comsig.sigWave(xf, Fs, t0 = - 1 / (2 * FB))
+
+
+
+
+
 def askrcvr(sig_rt, rtype, fcparms, FBparms, ptype, pparms):
     """
     Amplitude Shift Keying (ASK) Receiver for Coherent (’coh’) and Non-coherent (’noncoh’) ASK Signals
